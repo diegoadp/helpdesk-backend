9269fff0d3bede60acb794ba3bf187c6dd5e488d
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-08-22 19:49:36 -0300

    feat: add JPA annotations for domain classes

--- src/main/java/com/diegoadp/helpdesk/domain/Tecnico.java
+++ src/main/java/com/diegoadp/helpdesk/domain/Tecnico.java
@@ -1,18 +1,27 @@
 package com.diegoadp.helpdesk.domain;
 
+import com.diegoadp.helpdesk.domain.enums.Perfil;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa {
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
