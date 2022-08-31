96673a9c5e353371af879c70c475ec76fc0769d1
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-08-31 16:50:23 -0300

    feat: implement DTO pattern

--- src/main/java/com/diegoadp/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/diegoadp/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.diegoadp.helpdesk.resources;
 
 import com.diegoadp.helpdesk.domain.Tecnico;
+import com.diegoadp.helpdesk.dtos.TecnicoDTO;
 import com.diegoadp.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,8 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 }
