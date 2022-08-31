d7281a301fd14f1558f5c09ea2b59ddcccb7147c
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-08-31 12:17:48 -0300

    feat: protect app from serialization

--- src/main/java/com/diegoadp/helpdesk/domain/Tecnico.java
+++ src/main/java/com/diegoadp/helpdesk/domain/Tecnico.java
@@ -1,6 +1,7 @@
 package com.diegoadp.helpdesk.domain;
 
 import com.diegoadp.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
@@ -11,6 +12,7 @@ import java.util.List;
 public class Tecnico extends Pessoa {
     private static final long serialVersionUID = 1L;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
