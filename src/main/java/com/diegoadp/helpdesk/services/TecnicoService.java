c53dd22d6883adbd41950ab02aaa01c003725de2
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-09-01 10:26:13 -0300

    feat: create exception for object not found

--- src/main/java/com/diegoadp/helpdesk/services/TecnicoService.java
+++ src/main/java/com/diegoadp/helpdesk/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.diegoadp.helpdesk.services;
 
 import com.diegoadp.helpdesk.domain.Tecnico;
 import com.diegoadp.helpdesk.repositories.TecnicoRepository;
+import com.diegoadp.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id));
     }
 }
