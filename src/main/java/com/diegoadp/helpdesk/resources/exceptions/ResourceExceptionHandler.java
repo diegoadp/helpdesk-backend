fe091421760e8e54b009b37f4c3abf3c50f51cb1
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-09-01 12:33:41 -0300

    feat: create exception for CPF and Email already registered

--- src/main/java/com/diegoadp/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/diegoadp/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.diegoadp.helpdesk.resources.exceptions;
 
+import com.diegoadp.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.diegoadp.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,5 +19,15 @@ public class ResourceExceptionHandler {
                 "Object Not Found", ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+                                                                         HttpServletRequest request) {
+
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Violacao de dados", ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/diegoadp/helpdesk/services/TecnicoService.java
+++ src/main/java/com/diegoadp/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.diegoadp.helpdesk.services;
 
+import com.diegoadp.helpdesk.domain.Pessoa;
 import com.diegoadp.helpdesk.domain.Tecnico;
 import com.diegoadp.helpdesk.dtos.TecnicoDTO;
+import com.diegoadp.helpdesk.repositories.PessoaRepository;
 import com.diegoadp.helpdesk.repositories.TecnicoRepository;
+import com.diegoadp.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.diegoadp.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,9 @@ public class TecnicoService {
     @Autowired
     private TecnicoRepository repository;
 
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id));
@@ -27,7 +33,20 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDTO) {
         objDTO.setId(null);
+        validarPorCpfEEmail(objDTO);
         Tecnico newObj = new Tecnico(objDTO);
         return repository.save(newObj);
+    }
+
+    private void validarPorCpfEEmail(TecnicoDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if (obj.isPresent() && !obj.get().getId().equals(objDTO.getId())) {
+            throw new DataIntegrityViolationException("CPF ja cadastrado no sistema!");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if (obj.isPresent() && !obj.get().getId().equals(objDTO.getId())) {
+            throw new DataIntegrityViolationException("E-mail ja cadastro no sistema!");
+        }
     }
 }
